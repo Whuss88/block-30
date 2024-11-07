2e84c63c30621480a8c4ecfb4ce8d9a97536591e
Whuss88 <williamhussey95@gmail.com>
2024-11-07 10:48:30 -0500

    initial

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+
+const App = () => {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <h1>Book-Buddy</h1>
+    </>
+  )
+}
+
+export default App
