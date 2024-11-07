2cb9accbe49c982922d98dc5b6482d84cf3c1aef
Whuss88 <williamhussey95@gmail.com>
2024-11-07 12:21:18 -0500

    all book data fetched and displayed

---
+++ README.md
@@ -0,0 +1,3 @@
+## This app called book buddy will allow the user to see a catalog of books called from an api as well as interact with them, create, and manage an account.
+
+11/7 - all books fetched and displayed
--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,44 @@
 import { useState } from 'react'
 
-const App = () => {
-  const [count, setCount] = useState(0)
+import { useEffect } from "react"
 
+// view details on selected book 
+
+// login to an existing account
+
+// register new account
+// Tier 2(second phase)
+
+const App = () => {
+  const [Books, setBooks] = useState([])
+  // / display all books in catalog
+  // create func to async fetch within useEffect so it only runs once and when the state is updated
+  useEffect(()=>{
+    const fetchBooks =  async () => {
+      
+      const response = await fetch(`https://fsa-book-buddy-b6e748d1380d.herokuapp.com/api/books`)
+      const responseJson = await response.json() // brings back object called books
+      const allBooks = responseJson.books // brings back an array of 50 books
+      setBooks(allBooks) // sets the state of books
+      console.log(Books) // displayed updated state
+    }; 
+    fetchBooks()
+  }, []);
+  
   return (
     <>
       <h1>Book-Buddy</h1>
+      <section id='all-books'>
+      {
+        Books.map((singleBook)=>{
+          return (
+            <section key={singleBook.id}>
+              <h2>{singleBook.title}</h2>
+            </section>
+          )
+        })
+      }
+      </section>
     </>
   )
 }
