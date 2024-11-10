f04edc2ab7aba9e0479feaf285a417a06b110b0d
Whuss88 <williamhussey95@gmail.com>
2024-11-10 00:45:50 -0500

    register component has own page, functional home button, all user input can be console logged. Need to post user input to create user profile& receive token

--- src/App.jsx
+++ src/App.jsx
@@ -2,28 +2,28 @@ import { useState} from 'react'
 
 import Home from './Home.jsx'
 import Details from './Details.jsx'
+import Register from './Register.jsx'
 import { Route, Routes } from 'react-router-dom'
 
-// set paths for home and details 
-  // view details on selected book 
-    // function for all-book section
-      // setselectedBook in func 
+
 
 // login to an existing account
+  // create form 
 
-// register new account
+
 // Tier 2(second phase)
 
 const App = () => {
   const [selectedBook, setselectedBook] = useState ({})
   
-  console.log(selectedBook)
+  // console.log(selectedBook)
   return (
     <>
       <h1>Book-Buddy</h1>
       <Routes>
         <Route path='/' element={ <Home setselectedBook={setselectedBook}/>} />
         <Route path='/details'element={<Details selectedBook={selectedBook}/>} />
+        <Route path='/register' element={<Register />}/>
       </Routes>
       
      
